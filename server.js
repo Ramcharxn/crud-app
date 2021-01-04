dd4ffa62fa01305a50526264670943a0e3a3e77b
Ramcharxn <ramcharxn@gmail.com>
2021-01-04 11:28:34 +0530

    crud app

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const articleRouter = require('./routes/articles')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const methodOverride = require('method-override')
+const app = express()
+
+mongoose.connect('mongodb://localhost/blog',{
+    useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true
+})
+
+app.set('view engine','ejs')
+app.use(express.urlencoded({extended:false}))
+app.use(methodOverride('_method'))
+
+app.get('/',async(req,res) =>{
+    const articles =await Article.find().sort({
+        createdAt:'desc'
+    })
+    res.render('articles/index',{articles:articles})
+})
+
+app.use('/articles',articleRouter)
+
+const port = process.env.PORT || 5000
+
+app.listen(port,()=>{
+    console.log(`listening to port:${port}`)
+})
+
+/*app.use(express.static(__dirname));
+
+app.get('/',function(req,res){
+    res.render('index');
+})
+app.listen(port,function(){
+    console.log('running')
+})
+
+host online----not working*/
